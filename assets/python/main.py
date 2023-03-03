06e281eeac52516b55bf085623266859f1ff152e
Vince <vincentmarcelsantos@gmail.com>
2023-03-03 10:05:50 +0800

    some testing stuff

--- assets/python/main.py
+++ assets/python/main.py
@@ -2,9 +2,31 @@
 # python -m http.server 80 
 # in root folder
 # then localhost/index.html in a browser
+texts = {
+        'intro' : 'Hello, we are something',
+}
+
 def fac(x):
     if x <= 0: return 1
     return x * fac(x-1)
 
-x = fac(5)
-pyscript.write('python', x)
+def init():
+    """
+    Populate the texts dictionary
+    with function calls
+    """
+    global texts
+    texts['python'] = fac(5)
+
+def propagate_html():
+    """
+    Propagate the html page divs with the texts
+    """
+    for k in texts:
+        pyscript.write(k, texts[k])
+
+def main():
+    init()
+    propagate_html()
+
+main()
